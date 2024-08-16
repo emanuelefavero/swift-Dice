ff907cd2039d89c381752f8da68d9b82f57c6d07
emanuelefavero <emafaux89@gmail.com>
2024-08-16 07:00:55 +0200

    add animation code, UI and App Icon

--- swift-Dice/ViewController.swift
+++ swift-Dice/ViewController.swift
@@ -1,19 +1,54 @@
-//
-//  ViewController.swift
-//  swift-Dice
-//
-//  Created by Emanuele Favero on 16/08/24.
-//
+// ViewController.swift
+// swift-Dicee
 
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet var diceImageView1: UIImageView!
+  @IBOutlet var diceImageView2: UIImageView!
+  @IBOutlet var rollButton: UIButton!
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
+  let diceArray = [
+    #imageLiteral(resourceName: "DiceOne"),
+    #imageLiteral(resourceName: "DiceTwo"),
+    #imageLiteral(resourceName: "DiceThree"),
+    #imageLiteral(resourceName: "DiceFour"),
+    #imageLiteral(resourceName: "DiceFive"),
+    #imageLiteral(resourceName: "DiceSix")
+  ]
+
+  var animationTimer: Timer?
+
+  @IBAction func rollDice(_ sender: UIButton) {
+    startDiceAnimation() // start the animation
+
+    // Stop the animation and set the final dice images after 1 second
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+      self?.stopDiceAnimation()
+    }
   }
 
+  // Set what to do when the Main view is loaded
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
 
+  func startDiceAnimation() {
+    animationTimer?.invalidate() // invalidate previous timers if running
+
+    // Start a timer that changes the dice images every 0.1 seconds
+    animationTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
+      self?.diceImageView1.image = self?.diceArray.randomElement()
+      self?.diceImageView2.image = self?.diceArray.randomElement()
+    }
+  }
+
+  func stopDiceAnimation() {
+    animationTimer?.invalidate()
+    animationTimer = nil
+
+    // Set the final dice images
+    diceImageView1.image = diceArray.randomElement()
+    diceImageView2.image = diceArray.randomElement()
+  }
 }
-
