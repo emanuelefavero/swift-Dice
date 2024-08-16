b337244a6fd946e81c45f989baa5b0aaa478898c
emanuelefavero <emafaux89@gmail.com>
2024-08-16 06:42:19 +0200

    Initial Commit

---
+++ swift-Dice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-Dice
+//
+//  Created by Emanuele Favero on 16/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ swift-DiceUITests/swift_DiceUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  swift_DiceUITestsLaunchTests.swift
+//  swift-DiceUITests
+//
+//  Created by Emanuele Favero on 16/08/24.
+//
+
+import XCTest
+
+final class swift_DiceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
